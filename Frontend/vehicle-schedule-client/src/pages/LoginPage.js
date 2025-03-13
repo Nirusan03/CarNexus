d3033305f764f21e8c02b4e35d0ed69466d89806
Nirusan Hariharan <hariharan.20200094@iit.ac.lk>
2025-03-14 01:45:55 +0530

    [T7-3] Login form updated

--- Frontend/vehicle-schedule-client/src/pages/LoginPage.js
+++ Frontend/vehicle-schedule-client/src/pages/LoginPage.js
@@ -1,61 +1,7 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import "../styles/login.css";
+import LoginForm from "../components/LoginForm";
 
-const LoginForm = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const navigate = useNavigate();
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-
-    const response = await fetch("http://127.0.0.1:5000/auth/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
-    });
-
-    const data = await response.json();
-
-    if (response.ok) {
-      localStorage.setItem("token", data.access_token);
-      navigate("/home"); // Redirect to dashboard
-    } else {
-      setError(data.msg || "Login failed");
-    }
-  };
-
-  return (
-    <div className="login-container">
-      <div className="login-box">
-        <h2>Login</h2>
-        {error && <p style={{ color: "red" }}>{error}</p>}
-        <form onSubmit={handleLogin}>
-          <div className="input-group">
-            <label>Email</label>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="input-group">
-            <label>Password</label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <button type="submit">Login</button>
-        </form>
-      </div>
-    </div>
-  );
+const LoginPage = () => {
+  return <LoginForm />;
 };
 
-export default LoginForm;
+export default LoginPage;
