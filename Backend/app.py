6b5bd125ea1fd392e469fe8b1b304a017bc6762a
Nirusan Hariharan <hariharan.20200094@iit.ac.lk>
2025-03-14 01:17:08 +0530

    [B12] Server app.py updated

---
+++ Backend/app.py
@@ -0,0 +1,23 @@
+# app.py
+from flask import Flask
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+from db.mongo_connection import init_db
+import config
+from routes.auth_routes import auth_bp
+from routes.booking_routes import booking_bp
+
+app = Flask(__name__)
+app.config["JWT_SECRET_KEY"] = config.JWT_SECRET_KEY
+
+# Initialize extensions
+CORS(app)
+JWTManager(app)
+db = init_db(app)
+
+# Register Blueprints (Routes)
+app.register_blueprint(auth_bp, url_prefix="/auth")
+app.register_blueprint(booking_bp, url_prefix="/booking")
+
+if __name__ == "__main__":
+    app.run(debug=True)
