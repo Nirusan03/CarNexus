5a0e258ebac81080f77d6b473fb720733251acc1
Nirusan Hariharan <hariharan.20200094@iit.ac.lk>
2025-03-14 02:51:37 +0530

    [B8] Server file updated with service owner information

--- Backend/app.py
+++ Backend/app.py
@@ -6,6 +6,7 @@ from db.mongo_connection import init_db
 import config
 from routes.auth_routes import auth_bp
 from routes.booking_routes import booking_bp
+from routes.business_routes import business_bp
 
 app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = config.JWT_SECRET_KEY
@@ -18,6 +19,7 @@ db = init_db(app)
 # Register Blueprints (Routes)
 app.register_blueprint(auth_bp, url_prefix="/auth")
 app.register_blueprint(booking_bp, url_prefix="/booking")
+app.register_blueprint(business_bp, url_prefix="/business")
 
 if __name__ == "__main__":
     app.run(debug=True)
